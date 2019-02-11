ee813ad7071bdf6a3fb8670d6a5bcbdcf75dfa02
zarazi <zarazi@hotmail.com>
2019-02-11 21:49:31 +0700

    Redirect with alias if any

--- routes/redirector.js
+++ routes/redirector.js
@@ -4,7 +4,13 @@ const Url = require('../models/Url')
 module.exports = (router = new Router()) => {
   router.get('/:shortId', async (req, res) => {
     const { shortId } = req.params
-    const url = await Url.findOne({ shortId })
+    const selector = {
+      $or: [
+        { alias: { $exists: false }, shortId },
+        { alias: { $exists: true }, alias: shortId }
+      ]
+    }
+    const url = await Url.findOne(selector)
     if (url) {
       const { originalUrl } = url
       res.redirect(originalUrl)
