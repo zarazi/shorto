1adcf7320c3098846c566c6aa4b809891b0b5c78
zarazi <zarazi@hotmail.com>
2019-02-17 08:44:26 +0700

    Add basic validateOriginalUrl cases

--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -2,9 +2,24 @@ const { validateOriginalUrl } = require('../lib/validators')
 
 describe('Validation Tests', () => {
   describe('#validateOriginalUrl', () => {
-    test('should throw new Error()', async () => {
-      const validation = async _ => validateOriginalUrl()
-      await expect(validation()).rejects.toEqual(new Error())
+    describe('- negative cases:', () => {
+      ;[
+        [undefined, new Error('Undefined originalUrl')],
+        [null, new Error('Undefined originalUrl')]
+      ].forEach(([originalUrl, error]) => {
+        test(`should throw Error - ${error.message}`, async () => {
+          const expected = error
+          await expect(validateOriginalUrl(originalUrl)).rejects.toEqual(expected)
+        })
+      })
+    })
+
+    describe('- positive cases:', () => {
+      test('should pass with true response', async () => {
+        const originalUrl = 'http://abc.com'
+        const actual = await validateOriginalUrl(originalUrl)
+        expect(actual).toBeTruthy
+      })
     })
 
     // TODO: more tests
